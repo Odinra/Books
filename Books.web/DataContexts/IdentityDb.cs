bdcb2365aa1618029082ab7ad31cdef08ea235de
CK King <ckking70@gmail.com>
2015-07-16 22:48:00 -0500

    Initial Commit. Up to Scaffolding

---
+++ Books.web/DataContexts/IdentityDb.cs
@@ -0,0 +1,23 @@
+﻿using Books.web.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Books.web.DataContexts
+{
+    public class IdentityDb : IdentityDbContext<ApplicationUser>
+    {
+        public IdentityDb()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static IdentityDb Create()
+        {
+            return new IdentityDb();
+        }
+    }
+}
